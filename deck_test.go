e68e6029e831107fb144ae2368223167a623e546
Alberto Gil <agildav@gmail.com>
2019-03-01 11:15:53 -0400

    Added deal test

--- deck_test.go
+++ deck_test.go
@@ -6,8 +6,8 @@ import (
 	"testing"
 )
 
-// Validate a deck
-func validateDeck(d deck, t *testing.T) {
+// Validate a fresh new deck
+func validateFreshDeck(d deck, t *testing.T) {
 	// Have all 16 cards?
 	const nCrds = 16
 	if len(d) != nCrds {
@@ -29,7 +29,7 @@ func TestNewDeck(t *testing.T) {
 	d := newDeck()
 
 	// Validate the deck
-	validateDeck(d, t)
+	validateFreshDeck(d, t)
 }
 
 // Test if a deck gets saved / load - to / from a file
@@ -44,12 +44,31 @@ func TestSaveToFileAndNewDeckFromFile(t *testing.T) {
 	newDeck := newDeckFromFile(filename)
 
 	// Validate the deck
-	validateDeck(newDeck, t)
+	validateFreshDeck(newDeck, t)
 
 	// Remove file
 	err = os.Remove(filename)
 	if err != nil {
 		errMsg := fmt.Sprintf(":: could not remove file %s ::\n", filename)
+		t.Errorf(colorError(errMsg))
+	}
+}
+
+func TestDeal(t *testing.T) {
+	d := newDeck()
+
+	const nCrds = 5
+	hand, remainingCards := deal(d, nCrds)
+
+	// Have all the hand?
+	if len(hand) != nCrds {
+		errMsg := fmt.Sprintf(":: expected length %d, got %d ::\n", nCrds, len(hand))
+		t.Errorf(colorError(errMsg))
+	}
+
+	// Have all the remaining cards?
+	if len(remainingCards) != len(d)-nCrds {
+		errMsg := fmt.Sprintf(":: expected length %d, got %d ::\n", nCrds, len(remainingCards))
 		t.Errorf(colorError(errMsg))
 	}
 }
